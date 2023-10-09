3cb857f38bce6ac2393d2c0fccb24c0f9d028203
Ho Kim <ho.kim@ulagbulag.io>
2023-10-09 09:12:55 +0000

    Add nats uninstaller

---
+++ nats-uninstall.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# Copyright (c) 2023 Ho Kim (ho.kim@ulagbulag.io). All rights reserved.
+# Use of this source code is governed by a GPL-3-style license that can be
+# found in the LICENSE file.
+
+# Prehibit errors
+set -e -o pipefail
+
+###########################################################
+#   Configuration                                         #
+###########################################################
+
+# Configure default environment variables
+OPENARK_HOME_DEFAULT="${HOME}/Library/ipis-dev-env/netai-cloud"
+
+# Configure environment variables
+OPENARK_HOME="${OPENARK_HOME:-$OPENARK_HOME_DEFAULT}"
+
+###########################################################
+#   Main Function                                         #
+###########################################################
+
+for cluster_name in $("$(dirname "$0")/ceph-ls.sh"); do
+    echo -n "* ${cluster_name}: "
+    kubectl config use-context "${cluster_name}"
+
+    # Parse cluster name
+    export CLUSTER_NAME="$(
+        kubectl -n kube-system get configmap coredns -o yaml |
+            yq -r '.data.Corefile' |
+            grep -Po ' +kubernetes \K[\w\.\_\-]+'
+    )"
+
+    # Do not use PVC if no ceph cluster
+    if
+        ! kubectl \
+            --context "${cluster_name}" \
+            get namespace 'csi-rook-ceph' \
+            >/dev/null 2>/dev/null
+    then
+        export NATS_ENABLE_PVC="false"
+    else
+        export NATS_ENABLE_PVC="true"
+    fi
+
+    # Uninstall NATS
+    helm uninstall --namespace 'dash' nats || true
+    kubectl \
+        --context "${cluster_name}" \
+        --namespace 'dash' \
+        delete pvc 'nats-js-nats-0' || true
+done
+
+# Cleanup
+kubectl config use-context 'default'
+
+# Done
+exec echo 'Finished!'
