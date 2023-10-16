89a468be9eea95036e4fca3d9641f30df67806f6
Ho Kim <ho.kim@ulagbulag.io>
2023-10-16 06:54:14 +0000

    Add benchmark data csv dumper

---
+++ benchmark/visualize.py
@@ -0,0 +1,36 @@
+import os
+
+from openark import OpenArk
+from pandas import json_normalize, read_csv
+
+
+def load_data(output_file: str):
+    # Init OpenARK client
+    ark = OpenArk()
+
+    # Get model as DeltaTable
+    m = ark.get_model('performance-test-src')
+    df = m.to_delta().to_pandas()
+
+    # Flatten JSON data
+    df_timestamp = df['timestamp']
+    df_value = json_normalize([
+        value['data']
+        for value in df['value']
+    ])
+
+    df = df_value
+    df['timestamp'] = df_timestamp
+
+    # Write as .csv to be cached
+    df.to_csv('./output.csv')
+
+
+def load_csv(filename: str):
+    if not os.path.exists(filename):
+        load_data(filename)
+    return read_csv(filename)
+
+
+if __name__ == '__main__':
+    csv_data = load_csv('./output.csv')
