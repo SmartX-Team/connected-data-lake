1f92e90a27ecbd61c71ec8b8b2e611a89775b587
Ho Kim <ho.kim@ulagbulag.io>
2023-10-17 00:04:55 +0000

    Detach inspect_data function

--- benchmark/visualize.py
+++ benchmark/visualize.py
@@ -1,7 +1,7 @@
 import os
 
 from openark import OpenArk
-from pandas import json_normalize, read_csv, to_datetime, to_timedelta
+from pandas import DataFrame, json_normalize, read_csv, to_datetime, to_timedelta
 
 
 def load_data(output_file: str):
@@ -39,11 +39,35 @@ def load_data(output_file: str):
     df.to_csv(output_file)
 
 
-def load_csv(filename: str):
+def inspect_data(df: DataFrame, output_file: str):
+    # Group by metrics set
+    group = df.groupby(['data_size', 'messenger_type', 'payload_size'])
+    group_first = group.first()
+    group_last = group.last()
+
+    # Calculate duration
+    df = group_last
+    df['duration'] = (
+        to_datetime(group_last['timestamp'])
+        - to_datetime(group_first['timestamp'])
+        + to_timedelta('1s')
+    ).dt.total_seconds()
+
+    # Write as .csv to be cached
+    df.to_csv(output_file)
+
+
+def load_csv(filename: str, f):
     if not os.path.exists(filename):
         load_data(filename)
-    return read_csv(filename)
+    return f(filename)
 
 
 if __name__ == '__main__':
-    csv_data = load_csv('./output.csv')
+    inspected_data = load_csv(
+        './inspected.csv',
+        lambda filename: inspect_data(
+            load_csv('./raw.csv', read_csv),
+            filename,
+        )
+    )
