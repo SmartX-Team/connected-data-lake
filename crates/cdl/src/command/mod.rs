1953e333e7fefef85b881e4b9089482c91ba47c6
Ho Kim <ho.kim@ulagbulag.io>
2024-11-05 07:50:41 +0900

    fix: opt-out fuse support on non-linux kernels

--- crates/cdl/src/command/mod.rs
+++ crates/cdl/src/command/mod.rs
@@ -1,4 +1,5 @@
 pub mod copy;
+#[cfg(target_os = "linux")]
 pub mod mount;
 
 use anyhow::Result;
@@ -8,6 +9,7 @@ use clap::Subcommand;
 #[derive(Clone, Debug, PartialEq, Subcommand)]
 pub enum Command {
     Cp(self::copy::CopyArgs),
+    #[cfg(target_os = "linux")]
     Mount(self::mount::MountArgs),
 }
 
@@ -15,6 +17,7 @@ impl Command {
     pub(super) async fn execute(self, catalog: DatasetCatalog) -> Result<()> {
         match self {
             Self::Cp(args) => args.execute(catalog).await,
+            #[cfg(target_os = "linux")]
             Self::Mount(args) => args.execute(catalog).await,
         }
     }
